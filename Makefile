902705247afa6438ef28e1716f4b10a649d50d52
Carterahart18 <carterahart@gmail.com>
2020-03-08 20:15:23 -0700

    Move all generated files to dist

--- Makefile
+++ Makefile
@@ -1,9 +1,4 @@
 
 
 clean :
-	rm -f mnist.pkl
-	rm -f data.pkl
-	rm -f t10k-images-idx3-ubyte.gz
-	rm -f t10k-labels-idx1-ubyte.gz
-	rm -f train-images-idx3-ubyte.gz
-	rm -f train-labels-idx1-ubyte.gz
+	rm -f ./dist/*
--- mnistutil.py
+++ mnistutil.py
@@ -10,20 +10,20 @@ import os
 import numpy as np
 from PIL import Image
 
-output_dir = os.path.dirname(os.path.abspath(__file__))
-cache_file = "data.pkl"
+output_dir = os.path.dirname(os.path.abspath(__file__)) + '/dist'
+cache_file = 'data.pkl'
 img_size = 784  # TODO: Don't hard code ?
 url_base = 'http://yann.lecun.com/exdb/mnist/'
 files = {
-    'train_img': 'train-images-idx3-ubyte.gz',
-    'train_label': 'train-labels-idx1-ubyte.gz',
-    'test_img': 't10k-images-idx3-ubyte.gz',
-    'test_label': 't10k-labels-idx1-ubyte.gz'
+    'train_images': 'train-images-idx3-ubyte.gz',
+    'train_solutions': 'train-labels-idx1-ubyte.gz',
+    'test_images': 't10k-images-idx3-ubyte.gz',
+    'test_solutions': 't10k-labels-idx1-ubyte.gz'
 }
 
 
 def _get_file_dir(file_name):
-    return output_dir + "/" + file_name
+    return output_dir + '/' + file_name
 
 
 def _download_file(url, file_name):
@@ -38,12 +38,12 @@ def _download_file(url, file_name):
     file_path = _get_file_dir(file_name)
 
     if os.path.exists(file_path):
-        print("File", file_name, "already exists")
+        print('File', file_name, 'already exists')
         return
 
-    print("Downloading", file_name, "from", url, end=" ...\t")
+    print('Downloading', file_name, 'from', url, end=' ...\t')
     urllib.request.urlretrieve(url, file_path)
-    print("Done")
+    print('Done')
 
 
 def _load_solutions(file_name):
@@ -78,14 +78,14 @@ def _load_images(file_name):
     file_path = _get_file_dir(file_name)
 
     # Loads the image interpreting each byte as an unsigned int
-    print("Loading image", file_name, end="...\t")
+    print('Loading image', file_name, end='...\t')
     with gzip.open(file_path, 'rb') as f:
         data = np.frombuffer(f.read(), np.uint8, offset=16)
-    print("Done")
+    print('Done')
 
     # The initial format is a 1D array of pixels where every ${img_size} bytes
     # represents a single image
-    print("\t>", "Loaded", len(data), "pixels")
+    print('\t>', 'Loaded', len(data), 'pixels')
 
     # We'll split every ${img_size} bytes into it's own array. Each element of data is
     # now a ${img_size} length array representing one image. Reshape converts our array
@@ -96,16 +96,16 @@ def _load_images(file_name):
     total_images = len(data)
     image_size = len(data[0])
 
-    print("\t> Parsed", total_images, "images, each", image_size, "pixels")
+    print('\t> Parsed', total_images, 'images, each', image_size, 'pixels')
     return data
 
 
 def _load_dataset():
     dataset = {}
-    dataset['train_img'] = _load_images(files['train_img'])
-    dataset['train_label'] = _load_solutions(files['train_label'])
-    dataset['test_img'] = _load_images(files['test_img'])
-    dataset['test_label'] = _load_solutions(files['test_label'])
+    dataset['train_images'] = _load_images(files['train_images'])
+    dataset['train_solutions'] = _load_solutions(files['train_solutions'])
+    dataset['test_images'] = _load_images(files['test_images'])
+    dataset['test_solutions'] = _load_solutions(files['test_solutions'])
     return dataset
 
 
@@ -116,7 +116,7 @@ def _save_file(file_name, data):
 
 
 def _reshape_solution_to_bitmap(solution_array):
-    """
+    """"
     Parameters
     ----------
     solution_array: Array of numbers corresponding to image solutions
@@ -137,7 +137,7 @@ def _reshape_solution_to_bitmap(solution_array):
 
 
 def load_dataset(normalize=True, flatten=True, bitmapped=False):
-    if not os.path.exists(cache_file):
+    if not os.path.exists(_get_file_dir(cache_file)):
         # Download each file if necessary
         for value in files.values():
             _download_file(url_base + value, value)
@@ -145,28 +145,28 @@ def load_dataset(normalize=True, flatten=True, bitmapped=False):
         # Store data in binary file
         _save_file(cache_file, _load_dataset())
 
-    with open(cache_file, 'rb') as f:
+    with open(_get_file_dir(cache_file), 'rb') as f:
         dataset = pickle.load(f)
 
     if normalize:
         # Map image data from [0 to 255] to [0 to 1]
-        for key in ('train_img', 'test_img'):
+        for key in ('train_images', 'test_images'):
             dataset[key] = dataset[key].astype(np.float32)
             dataset[key] /= 255.0
 
     if not flatten:
         # Convert images to 28 x 28 arrays
         # TODO: Get rid of second dimension ?
-        for key in ('train_img', 'test_img'):
+        for key in ('train_images', 'test_images'):
             dataset[key] = dataset[key].reshape(-1, 1, 28, 28)
 
     if bitmapped:
-        for key in ('train_label', 'test_label'):
+        for key in ('train_solutions', 'test_solutions'):
             dataset[key] = _reshape_solution_to_bitmap(dataset[key])
 
     return (
-        (dataset['train_img'], dataset['train_label']),
-        (dataset['test_img'], dataset['test_label']))
+        (dataset['train_images'], dataset['train_solutions']),
+        (dataset['test_images'], dataset['test_solutions']))
 
 
 load_dataset()
