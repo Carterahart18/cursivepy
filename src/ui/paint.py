16f37653f6059256a96e033acc2a19c34c9552a1
Carterahart18 <carterahart@gmail.com>
2020-03-13 00:29:16 -0700

    Flesh out drawing program
    * Each frame connects a line from prev_coors to coors
    * A brush size can draw a circle around the brush

--- src/ui/paint.py
+++ src/ui/paint.py
@@ -33,24 +33,11 @@ class Paint(object):
         self.canvas.grid(row=1, columnspan=5)
 
     def setup_fields(self):
-        self.pen_size = 5
+        self.prev_col = None
+        self.prev_row = None
+        self.brush_size = 2
         self.canvas.bind('<B1-Motion>', self.paint)
         self.canvas.bind('<ButtonRelease-1>', self.reset)
-
-    def clear(self):
-        self.canvas.delete("all")
-        self.image_data = numpy.zeros((IMAGE_SIZE, IMAGE_SIZE))
-
-    def paint(self, event):
-        x = int(event.x // PIXEL_SIZE)
-        y = int(event.y // PIXEL_SIZE)
-
-        self.image_data[y][x] = 1
-
-        self.draw_image(event)
-
-        self.prev_x = x
-        self.prev_y = y
 
     def draw_image(self, event):
         self.canvas.delete("all")
@@ -67,9 +54,67 @@ class Paint(object):
                                              fill=value,
                                              outline=value)
 
+    def brush(self, row, col):
+        radius = self.brush_size / 2
+        min_row = int(row - radius)
+        max_row = int(row + radius + 1)
+        min_col = int(col - radius)
+        max_col = int(col + radius + 1)
+        for i in range(min_row, max_row):
+            for j in range(min_col, max_col):
+                # Pythagorean theorum to get distance from center
+                row_diff = i - row
+                col_diff = j - col
+                dist = math.sqrt(pow(row_diff, 2) + pow(col_diff, 2))
+
+                if dist < radius:
+                    self.image_data[i][j] = 1
+                elif dist < radius + 1:
+                    self.image_data[i][j] = \
+                        max(radius + 1 - dist, self.image_data[i][j])
+        pass
+
+    def paint(self, event):
+        num_fill_points = 10
+        col = int(event.x // PIXEL_SIZE)
+        row = int(event.y // PIXEL_SIZE)
+
+        if col >= IMAGE_SIZE or col < 0:
+            return
+        if row >= IMAGE_SIZE or row < 0:
+            return
+
+        if self.prev_col != None and self.prev_row != None:
+            col_diff = col - self.prev_col
+            row_diff = row - self.prev_row
+
+            dcol = col_diff / (num_fill_points + 1)
+            drow = row_diff / (num_fill_points + 1)
+
+            pointSet = set([])
+            for i in range(1, num_fill_points + 1):
+                col_point = int(self.prev_col + dcol * i)
+                row_point = int(self.prev_row + drow * i)
+                pointSet.add((row_point, col_point))
+
+            for point in pointSet:
+                self.brush(point[0], point[1])
+                self.image_data[point[0]][point[1]] = 1
+        else:
+            self.brush(row, col)
+
+        self.prev_col = col
+        self.prev_row = row
+
+        self.draw_image(event)
+
+    def clear(self):
+        self.canvas.delete("all")
+        self.image_data = numpy.zeros((IMAGE_SIZE, IMAGE_SIZE))
+
     def reset(self, event):
-        self.prev_x = None
-        self.prev_y = None
+        self.prev_col = None
+        self.prev_row = None
 
 
 if __name__ == '__main__':
